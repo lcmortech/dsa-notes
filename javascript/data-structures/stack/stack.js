3e9255e4de21cbe467360c1eff9d041bd3a2a693
Lauren <rg326@live.com>
2021-10-13 17:56:54 -0400

    added peek method, additional notes

--- javascript/data-structures/stack/stack.js
+++ javascript/data-structures/stack/stack.js
@@ -13,7 +13,7 @@ class Stack {
     push(element){
         this.items[this.count] = element
         this.count++ //increases/lengthens array
-        console.log(object)
+        console.log(element)
         return this.count - 1
     }
     
@@ -23,8 +23,14 @@ class Stack {
         if(this.count === 0) return undefined
         let deleteItem = this.items[this.count]
         this.count-- //decreases/shortens array
-        
+        console.log(`${deleteItem} removed`)
         return deleteItem
+    }
+    
+    //Check top element in stack
+    peek(){
+        console.log(`Top element is ${this.items[this.count - 1]}`)
+        return this.items[this.count - 1]
     }
     
     
@@ -39,9 +45,27 @@ class Stack {
 //that index is then set to the element passed through 'push'
 //adds 100,200, and 300 to items array, respectively
 
+//Breakdown: Pop
+//aka: a conditional is checked to see is the current index is empty, or equal to 0. If true, return undefined, as well as decrease the index by one. Then, return the current index. 
+//first line - the conditional statement
+//second line - placing the index or count of the array in the variable to be returned/removed.
+//third line - decrementing to previous index/count by one
+//returning/removing the current index
+
+
+
+//Test cases
 const stack = new Stack()
 
 
 stack.push(100)
 stack.push(100)
 stack.push(100)
+
+stack.peek()
+
+stack.pop()
+stack.pop()
+
+console.log(stack.pop())
+console.log(stack.push())
