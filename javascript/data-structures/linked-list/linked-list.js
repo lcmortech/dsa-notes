f1c66d980cd8abd29e14cc0117468536266940be
Lauren <rg326@live.com>
2021-10-14 19:39:11 -0400

    set up linked list structure

---
+++ javascript/data-structures/linked-list/linked-list.js
@@ -0,0 +1,22 @@
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+    
+    //Insert first node
+    
+    
+    
+    //Insert last node
+    
+    
+    
+    
+    //Insert at index
+    
+    
+    
+    
+    //Remove at index
+}
