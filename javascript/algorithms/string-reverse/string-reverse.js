d1379e0692f99fa7f4de6f3f28145a0e0c172329
Lauren <rg326@live.com>
2021-10-28 17:59:58 -0400

    added more notes to string reversal

--- javascript/algorithms/string-reverse/string-reverse.js
+++ javascript/algorithms/string-reverse/string-reverse.js
@@ -1,4 +1,4 @@
-//Method 1:In-Built Method
+//Method 1: In-Built Method
 
 //- Create a function that takes in a string parameter (str)
 // - call split(' ') method on the str
@@ -6,3 +6,14 @@
 // - finally call join ('') on the str
 // - the test should pass
 
+//Method 2: Iterative/For Loop Method
+
+//- Create a function that takes in a string parameter (str)
+// - declare an empty string variable which will hold the reversed string (let rev = '')
+//- initialize a for loop ("for of" loop preferred)
+//- for each iteration, loop over each character of the string, which acts like an array (let char of str)
+// - add each looped character to the empty string "rev" via concatenation (rev = char + rev, or rev += char)
+// - return the final reversed string (return rev)
+// - the test should pass
+
+//Method 3: Reduction Method, using Reducer Method
