e09fd70f5abccbc71d6de82ecdb081ace2db6656
Lauren <rg326@live.com>
2021-10-28 17:52:31 -0400

    added algo notes on strings

---
+++ javascript/algorithms/string-reverse/string-reverse.js
@@ -0,0 +1,8 @@
+//Method 1:In-Built Method
+
+//- Create a function that takes in a string parameter (str)
+// - call split(' ') method on the str
+// - call reverse() method on the str
+// - finally call join ('') on the str
+// - the test should pass
+
