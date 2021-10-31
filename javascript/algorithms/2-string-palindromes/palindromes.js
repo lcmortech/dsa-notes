15f7b0325e4392b9e2131101dc4b0a22fb0b99bf
Lauren <rg326@live.com>
2021-10-30 20:14:58 -0400

    added one method for palindromes

--- javascript/algorithms/2-string-palindromes/palindromes.js
+++ javascript/algorithms/2-string-palindromes/palindromes.js
@@ -1,2 +1,13 @@
+//Method 1
+
+// - Create a function that takes in a string for its parameter
+// - Declare a variable and within it, store the string with the following methods: split, reverse, join
+// - Compare the string passed in to the variable reverse, then return (str === rev)
+// - If the string is a palindrome, it will evaluaate to True, if not, False
+// - the test should pass
+
+
+
+
 // - using "i" gives us access to the characters in the array,we increment for every
 // the extra - 1 takes into account the zero indexing of js arrays
