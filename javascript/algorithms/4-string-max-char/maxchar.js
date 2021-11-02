0af5fa13e88e55a0d9637b3b2bd31a93f1009a79
Lauren <rg326@live.com>
2021-11-01 23:26:16 -0400

    added notes on solving maxchar

---
+++ javascript/algorithms/4-string-max-char/maxchar.js
@@ -0,0 +1,19 @@
+//Related Questions:
+// - What is the most common character in a string?
+// - Does string A have the same characters a string B (is it an anagram)?
+// - Does the given string have any repeated characters in it?
+// - Anything around counting the number or occurences of characters or comparing the number of characters between two strings
+
+// - We will essentially convert the string to an object/hash/map where the properties/keys are the characters in the string
+// - The values are the amount of times that particular character occurs
+
+//Ex:
+
+//"Hello"
+
+//{
+//    H: 1,
+//    e: 1,
+//    l: 2,
+//    o: 1,
+//}
