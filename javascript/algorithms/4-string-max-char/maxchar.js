8305cdb734b5253c04a58b5132218a23e35caed2
Lauren <rg326@live.com>
2021-11-02 20:14:14 -0400

    set up method one for maxchar

--- javascript/algorithms/4-string-max-char/maxchar.js
+++ javascript/algorithms/4-string-max-char/maxchar.js
@@ -1,3 +1,5 @@
+//Method 1: Using a Object/Hash/Map
+
 //Related Questions:
 // - What is the most common character in a string?
 // - Does string A have the same characters a string B (is it an anagram)?
@@ -17,3 +19,29 @@
 //    l: 2,
 //    o: 1,
 //}
+
+//Steps for Method 1 Data Structure:
+// - Create a string (const = 'Hello')
+// - Iterate through that string, and for every character, add it to a new object (const chars = {};)
+// - Either add the character as a new property and assign it a value of 1, or if we've seen it already, add 1 to it/increment it by 1
+// - For every char we find, we add a property to "chars"
+
+//for (let char of string) {
+//    if(!chars[char]) {
+//        chars[char] = 1;
+//    } else {
+//        chars[char]++;
+//    }
+//}
+
+//chars;
+
+//ALT
+//for (let char of string) {
+//   chars[char] = chars[char] + 1 || 1;
+//}
+// chars;
+
+
+//       
+// - Doing this, we end up with our object
