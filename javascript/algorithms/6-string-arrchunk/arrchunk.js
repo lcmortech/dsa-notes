9d94c3cec3d69f7a49592bad8ea4fdd6acfd9c3a
Lauren <rg326@live.com>
2021-11-06 16:52:27 -0400

    added steps for array chunking

---
+++ javascript/algorithms/6-string-arrchunk/arrchunk.js
@@ -0,0 +1,9 @@
+//Array Chunking
+
+//Steps at a glance:
+// - Create empty array variable to hold chunks
+// - Using a for loop ('for each element'), retrieve each element in the 'unchunked' array.
+// - Retrieve the last element in 'chunked'.
+// - Check to see if the last element does not exist, or if its length is equal to chunk size. 
+// - If so(if), push a new chunk into 'chunked' with the current element.
+// -  Otherwise(else), add the current element into the chunk.
