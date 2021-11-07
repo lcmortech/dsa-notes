79b28d3b618ec302b01a2f087cc5b79dcbc06bdf
Lauren <rg326@live.com>
2021-11-07 13:22:45 -0500

    added alt explanation for chunk array

--- javascript/algorithms/6-string-arrchunk/arrchunk.js
+++ javascript/algorithms/6-string-arrchunk/arrchunk.js
@@ -22,3 +22,12 @@ const chunkArray = (arr) => {
     }
 } */
 
+//Method 1 In-Depth:
+// - We assume our chunk function gets called on an array with 3 elements inside
+// - We begin to iterate over our original unchunked array
+// - For each element in unchunked array:
+// - Retrieve the last element in the new chunked array (will be empty the first time around)
+// - If it is indeed empty/doesn't exist OR equal to chunked size, we will push a new subarray/chunk into the chunk array,
+// - along with the current char/element we're iterating through inside the new subarray/chunk
+// - In our next iteration, we go down to the else case: add the current element into the new subarray/chunk,
+// - making it the last element in our new chunk subarray
