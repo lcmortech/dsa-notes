2d19bd8a4e3124d3eb0fd5bedc3b4f9b9baef11e
Lauren <rg326@live.com>
2021-11-09 18:16:08 -0500

    added complete solution for array chunk

--- javascript/algorithms/6-string-arrchunk/arrchunk.js
+++ javascript/algorithms/6-string-arrchunk/arrchunk.js
@@ -29,6 +29,23 @@ const chunkArray = (arr) => {
 // - Retrieve the last element in the new chunked array (will be empty the first time around)
 // - If it is indeed empty/doesn't exist OR equal to chunked size, we will push a new subarray/chunk into the chunk array,
 // - along with the current char/element we're iterating through inside the new subarray/chunk
-// - In our next iteration, we go down to the else case: add the current element into the new subarray/chunk,
+// - In our next iteration, we go down to the else case (current chunk not yet full): add the current element into the new subarray/chunk,
 // - making it the last element in our new chunk subarray
 // - the test should pass
+
+//Example:
+//function chunk(arr, size) {
+//    const chunked = [];
+//    
+//    for (let element of arr) {
+//        const last = chunked[chunked.length - 1];
+//        
+//        if(!last || last.length === size) {
+//            chunked.push([element]);
+//        } else {
+//            last.push(element);
+//        }
+//    }
+//    
+//    return chunked;
+//}
