7eee23d823b561959dedae72ebb67905ec75495a
Lauren <rg326@live.com>
2021-11-11 21:33:30 -0500

    added notes for first method for anagrams

---
+++ javascript/algorithms/7-string-anagrams/anagram.js
@@ -0,0 +1,9 @@
+//Method one
+// - Create a function that takes two strings as params
+// - Create a character map for the two strings and compare them
+// - Create a helper function buildCharMap that takes one string param
+// - In the helper function, we declare an object variable called "charMap"
+// - Create a for loop let char of str.replace(), whose method takes the reg x statement:/[^\w/g, '']
+// - attach the method .toLowerCase to the same str.replace
+
+
