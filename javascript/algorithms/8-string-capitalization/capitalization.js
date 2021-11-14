e209d18534bc5c56e4eea25d5bee6bc594dcead0
Lauren <rg326@live.com>
2021-11-14 17:10:26 -0500

    added string capitalization

---
+++ javascript/algorithms/8-string-capitalization/capitalization.js
@@ -0,0 +1,9 @@
+//Methods used:
+//string.slice(index start, index end(ex)) - if end not provided, the rest of the string is automatically included
+//word[0].toUppercase()
+
+
+
+function capitalization(str) {
+
+}
