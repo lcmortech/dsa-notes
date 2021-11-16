56178584da8b079643f505c95866d89a554aec6a
Lauren <rg326@live.com>
2021-11-15 20:02:02 -0500

    added second method explanation

--- javascript/algorithms/8-string-capitalization/capitalization.js
+++ javascript/algorithms/8-string-capitalization/capitalization.js
@@ -14,11 +14,20 @@
 //Join "words" into a string and return it
 
 
-function capitalization(str) {
-    let words = [];
-    for (let word of str.split(' ')){
-        let cap = word[0].toUpperCase() + word.slice(1)
-        words.push(cap);
-    }
-    return words.join(' ');
-}
+//Method one example:
+//function capitalization(str) {
+//    let words = [];
+//    for (let word of str.split(' ')){
+//        let cap = word[0].toUpperCase() + word.slice(1)
+//        words.push(cap);
+//    }
+//    return words.join(' ');
+//}
+
+//Method two:
+// - Create a function that takes in a string
+// - Create an empty string called 'result'
+// - For each character in the string:
+    // - If the character is to the left of a space,
+    // - Capitalize it and add it to the 'result'
+    // - Else, add it to 'result'
