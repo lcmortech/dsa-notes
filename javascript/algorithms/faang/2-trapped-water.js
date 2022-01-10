9c53754b24e49ecd3d5a3306be380ec2f191fad5
LC Morgan <rg326@live.com>
2022-01-09 23:50:14 -0500

    more notes

--- javascript/algorithms/faang/2-trapped-water.js
+++ javascript/algorithms/faang/2-trapped-water.js
@@ -1,1 +1,3 @@
 //Figure out two arguments to find out the container with the most water, using an array of positive integers.
+
+//Use bar chart to calculate the indices holds the most water by area.
