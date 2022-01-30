685ad2c0579c9d866c2f84bc1bc0d51d33e49754
Lauren <rg326@live.com>
2022-01-30 17:59:52 -0500

    added traversal methods for linked list

--- javascript/algorithms/structy/linkedList.js
+++ javascript/algorithms/structy/linkedList.js
@@ -4,3 +4,31 @@ class Node {
         this.next = null
     }
 }
+
+let a = new Node('A')
+let b = new Node('B')
+let c = new Node('C')
+let d = new Node('D')
+
+a.next = b
+b.next = c
+c.next = d
+
+/*const listTraversal = (head)=>{
+    let current = head
+    while(current !== null){
+        console.log(current.val)
+        current = current.next
+    }
+}*/
+
+const listTraversal = (head)=>{ //parameter
+    if(head === null){
+        return;
+    } else {
+        console.log(head.val)
+        listTraversal(head.next) //argument
+    }
+}
+
+listTraversal(a)
