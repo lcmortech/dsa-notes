1ecc42b09470d28b8c746bbf61a9e55ad6578a94
Lauren <rg326@live.com>
2022-02-01 18:48:40 -0500

    added one solution for uncompress

--- javascript/algorithms/structy/array-problems/1-uncompress.js
+++ javascript/algorithms/structy/array-problems/1-uncompress.js
@@ -1,16 +1,25 @@
-const compress = (str) => {
+const uncompress = (str) => {
     nums = '0123456789' //for reference
-    const result = ''
-    let i = 0
+    let result = ''
+    let i = 0 //prints out the chars via concat + iteration
     let j = 0 //iterates over the nums, ends at first char
 
     while (j < str.length){
-        if(nums.includes(s[j])){
-
+        if(nums.includes(str[j])){
+            j += 1
         } else { //fires at the end of the number
-            const num = Number(s.slice(i, j)) //(starts at i, ends bf j)
+            const num = Number(str.slice(i, j)) 
+            //(starts at i ends bf j) - results in just the number
+            for(let count = 0; count < num; count += 1) {
+                result += str[j] //concat j to string
+            }
+            j += 1 //iterates over and updates j
+            i = j //updates i to j's values
         }
     }
 
     return result
 }
+
+//concat = O(n) time complexity
+//push = O(1) time complexity
