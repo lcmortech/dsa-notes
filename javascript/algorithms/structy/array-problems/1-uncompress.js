a04084cadccf029cdcf5e236a42eb901eabfc026
Lauren <rg326@live.com>
2022-02-01 18:48:40 -0500

    added uncompress problem for arrays

---
+++ javascript/algorithms/structy/array-problems/1-uncompress.js
@@ -0,0 +1,16 @@
+const compress = (str) => {
+    nums = '0123456789' //for reference
+    const result = ''
+    let i = 0
+    let j = 0 //iterates over the nums, ends at first char
+
+    while (j < str.length){
+        if(nums.includes(s[j])){
+
+        } else { //fires at the end of the number
+            const num = Number(s.slice(i, j)) //(starts at i, ends bf j)
+        }
+    }
+
+    return result
+}
