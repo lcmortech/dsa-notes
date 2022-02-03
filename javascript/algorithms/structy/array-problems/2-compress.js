44f8f27e6d7ea9d22f81d03178c3b616525f446e
Lauren <rg326@live.com>
2022-02-02 23:00:49 -0500

    added optimized solution for compress problem using array, instead of string

--- javascript/algorithms/structy/array-problems/2-compress.js
+++ javascript/algorithms/structy/array-problems/2-compress.js
@@ -20,6 +20,47 @@ const compress = (s) => {
     return result
 };
 
+//Optimized solution O(n)
+/*
+const compress = (s) => {
+  let result = []
+  let i = 0 //start of streak
+  let j = 0 //end of streak
+  let char
+  
+  while(j <= s.length){
+    if(s[i]===s[j]){ //check equality shows streak start
+      j += 1
+    } else {
+      num = j - i //distance from pointers indicate chars
+      if(num === 1){
+        result.push(s[i])
+      } else {
+      result.push(num, s[i])
+    }
+      i = j
+  }
+  }
+    return result.join('')
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  compress
+};
+
+*/
 
 
 
