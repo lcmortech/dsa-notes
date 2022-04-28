937dc5a9ade836dae66cb6f4906aada42ab9b3e1
Lauren <rg326@live.com>
2022-04-28 18:16:57 -0400

    added linked list under the hood file to dsa-notes

---
+++ scott-barrett/js/linked-lists/under-the-hood.js
@@ -0,0 +1,28 @@
+/*
+- Node: An object that consists of the value and the pointer
+{
+    value: 4,
+    next: null
+}
+
+Complete LL Structure:
+{
+    head: {
+        value: 11,
+        next: {
+            value: 3,
+            next: {
+                value: 23,
+                next: {
+                    value: 7,
+                    next: {
+                        value: 4,
+                        next: null
+                    }
+                }
+            }
+        }
+    }
+}
+*/
+
