9398950d5de9753226154fe13507f600a664daec
Lauren <rg326@live.com>
2022-04-30 19:03:28 -0400

    constucted push method for linked list

---
+++ scott-barrett/js/linked-lists/push.js
@@ -0,0 +1,35 @@
+/*
+- create a new node
+- the last node point to it
+- have tail point to it
+- if it has no nodes, then have head and tail point to it 
+*/
+
+class NodeClass {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor(value){
+        const newNode = new NodeClass(1)
+        this.head = newNode
+        this.tail = newNode
+        this.length = 1
+    }
+
+    push(value){
+        const newNode = new NodeClass(value)
+        this.head = newNode
+        if (!this.head){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next === newNode
+        }
+        this.length += 1
+        return this //this entire linkedlist w/ node add
+    }
+}
