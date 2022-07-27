d25df89ae8ea7ad94c632a71043a594a2f97701d
Lauren <rg326@live.com>
2022-07-27 17:22:15 -0400

    added info to bt-insert-node

--- javascript/algorithms/structy/bt-insert-node.js
+++ javascript/algorithms/structy/bt-insert-node.js
@@ -1,3 +1,20 @@
+/*
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+insertNode(a, 'x', 2);
+// a -> b -> x -> c -> d
+*/
+
+
 const insertNode = (head, value, index) => {
   if (index === 0) {
     const newHead = new Node(value);
