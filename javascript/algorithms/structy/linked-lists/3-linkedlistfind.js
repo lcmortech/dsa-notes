57cdac001b57edbf0e2ebeb0084297eed6948ee8
Lauren <rg326@live.com>
2022-08-04 20:48:02 -0400

    added solutions to linked list find in structy

---
+++ javascript/algorithms/structy/linked-lists/3-linkedlistfind.js
@@ -0,0 +1,16 @@
+//Iterative
+const linkedListFind = (head, target) => {
+  let current = head;
+  while (current !== null) {
+    if (current.val === target) return true;
+    current = current.next;
+  }
+  return false;
+};
+
+//Recursive
+const linkedListFind = (head, target) => {
+  if (head === null) return false;
+  if (head.val === target) return true;
+  return linkedListFind(head.next, target);
+};
