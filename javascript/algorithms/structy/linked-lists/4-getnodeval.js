6e557f03e350f0f0e52483457fba4ddf5f04af66
Lauren <rg326@live.com>
2022-08-05 22:48:03 -0400

    added solution to get node value in structy

--- javascript/algorithms/structy/linked-lists/4-getnodeval.js
+++ javascript/algorithms/structy/linked-lists/4-getnodeval.js
@@ -1,4 +1,18 @@
 //Iterative
-
+const getNodeValue = (head, index) => {
+  let count = 0;
+  let current = head;
+  while (current !== null) {
+    if (count === index) return current.val;
+    current = current.next;
+    count += 1;
+  }
+  return null;
+};
 
 //Recursive
+const getNodeValue = (head, index) => {
+  if (head === null) return null;
+  if (index === 0) return head.val;
+  return getNodeValue(head.next, index - 1);
+};
