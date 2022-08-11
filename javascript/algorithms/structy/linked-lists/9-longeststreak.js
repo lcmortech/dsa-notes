61d5371ac38ae00edaa9845ab0d7c59ca6a8b0ca
Lauren <rg326@live.com>
2022-08-10 23:03:38 -0400

    added longest streak to structy

---
+++ javascript/algorithms/structy/linked-lists/9-longeststreak.js
@@ -0,0 +1,24 @@
+//Iterative
+const longestStreak = (head) => {
+  let maxStreak = 0;
+  let currentStreak = 0;
+  let currentNode = head;
+  let prevVal = null;
+  
+  while (currentNode !== null) {
+    if (currentNode.val === prevVal) {
+      currentStreak += 1;
+    } else {
+      currentStreak = 1;
+    }
+    
+    if (currentStreak > maxStreak) {
+      maxStreak = currentStreak;
+    }
+    
+    prevVal = currentNode.val;
+    currentNode = currentNode.next;
+  }
+  
+  return maxStreak;
+};
