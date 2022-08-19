633fe36ef36a7fc3139cf510faa0682b887c618b
Lauren Morgan <rg326@live.com>
2022-08-18 20:14:56 -0400

    added explanation for optimized solution

---
+++ ztm-faang/arrays/1c-two-sum-optimized.js
@@ -0,0 +1,33 @@
+//Index = 0,1,2,3,4
+//nums = [1,3,7,9,2] target = 11
+
+//Initialize hashmap
+//We want to calculate the nTF at p1 (target - nums[p1])
+//We want to set the numToFind as the key
+//We want to store each nTF as the key
+//We will first store 10 in our hashmap
+//We then store 8 in the hashmap
+//We then store 4 in the hashmap
+//We then finally store 2 in the hashmap
+//we then store the indexes for each nTF as their corresponding value in the hashmap
+/*
+{
+    10:0, (1)
+    8: 1, (3)
+    4: 2, (7)
+    2: 3, (9)
+}
+*/
+
+//Return the value in the last value in the hashmap, as well as the current value of p1 to get the solution
+
+
+
+
+
+
+
+// const findTwoSum = function (array, target) {
+//     const hashMap = {}
+
+//     const findNum = target - array
