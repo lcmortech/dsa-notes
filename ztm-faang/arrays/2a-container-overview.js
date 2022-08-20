5e0236b4710b8ffc96578f469e7eaf6ca749507b
Lauren <rg326@live.com>
2022-08-19 21:23:25 -0400

    added container with most water overview

---
+++ ztm-faang/arrays/2a-container-overview.js
@@ -0,0 +1,13 @@
+//Container with Most Water Overview
+//We must return the area with the most water
+
+/*Verify Constraints
+1. Does the thickness of the lines affect the area?
+No, assume they take up no space
+
+2. Do the left and right sides count as walls?
+No, sides cannot be used to form a container
+
+3. Does a higher line inside our container affect our area?
+No, lines inside a container don't affect the area
+*/
