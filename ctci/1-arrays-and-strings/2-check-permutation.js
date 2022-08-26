62aee789f0f212eeeebf09fbfaae374438d0cf62
Lauren Morgan <rg326@live.com>
2022-08-25 21:40:37 -0400

    added sol 2 to check permutation

--- ctci/1-arrays-and-strings/2-check-permutation.js
+++ ctci/1-arrays-and-strings/2-check-permutation.js
@@ -1,5 +1,4 @@
-//SOL #1
-//First - Sort the string
+//SOL #1 - Sort the strings
 function sort(str){
     let content = s.toCharArray()
     sort(content)
@@ -11,4 +10,17 @@ function permutation(s, t){
         return false
     }
     return sort(s).equals(sort(t))
+}
+
+//SOL #2 - Check if the two strings have identical character counts
+function permutation(s, t) {
+    if(s.length() != t.length()) return false
+
+    let letters = 128
+    for(let i = 0; i < s.length; i++){
+        if(letters[t.charAt(i)] < 0){
+            return false
+        }
+    }
+    return true //since letters has no neg values, it has no positive values either
 }
