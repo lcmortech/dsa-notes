d9b8a98473514d7f41e533639ce6ed2849cdeb85
Lauren Morgan <rg326@live.com>
2022-08-27 22:21:10 -0400

    initial commit bae-dsa

---
+++ bae-dsa/js-strings.js
@@ -0,0 +1,11 @@
+//String Access
+
+//charAt()/charCodeAt()
+//'dog'.chareAt(1) -> 'o'
+//'dog'.charCodeAt(o) -> 1
+
+//Substring -> (start index, end index)
+//'Youtube'.substring(1,2) -> 'o'
+//Substring -> (start index) (doesn't end till string ends)
+//'Youtube'.substring(1) -> 'outube'
+
