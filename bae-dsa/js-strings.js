ec05dbb32822cf57454d3877022570dd1769332a
Lauren Morgan <rg326@live.com>
2022-08-28 23:17:53 -0400

    added more search string methods to bae dsa

--- bae-dsa/js-strings.js
+++ bae-dsa/js-strings.js
@@ -15,7 +15,9 @@ var a = 'a'
 var b = 'b'
 console.log(a < b) prints true
 
-String Search - To find a specific string within a string, you can use .indexOf(searchValue[, fromIndex]). This takes a parameter that is the string to be searched as well as an optional parameter for the starting index for the search. It returns the position of the matching string, but if nothing is found, then -1 is returned. Note that this function is case sensitive.
+String Search - 
+
+indexOf: To find a specific string within a string, you can use .indexOf(searchValue[, fromIndex]). This takes a parameter that is the string to be searched as well as an optional parameter for the starting index for the search. It returns the position of the matching string, but if nothing is found, then -1 is returned. Note that this function is case sensitive.
 
 'Red Dragon'.indexOf('Red') // returns 'o'
 'Red Dragon'.indexOf('Red'); // returns 0
@@ -24,5 +26,9 @@ String Search - To find a specific string within a string, you can use .indexOf(
 'Red Dragon'.indexOf('Dragon', 4); // returns 4
 'Red Dragon'.indexOf(", 9); // returns 9
 
-
+startsWith()/endsWith(): Finally, startsWith returns true (boolean) if the string starts with the specified input, and endsWith checks whether the string ends with the specified input.
+'Red Dragon'.startsWith('Red'); // returns true
+'Red Dragon'.endsWith('Dragon'); // returns true
+'Red Dragon'.startsWith('Dragon'); // returns false
+'Red Dragon'.endsWith('Red'); // returns false
 */
