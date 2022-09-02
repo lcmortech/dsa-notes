4397f9a70d21f4e8b4ad376f42941e4f247a542f
Lauren Morgan <rg326@live.com>
2022-09-01 21:37:38 -0400

    added string replace

---
+++ bae-dsa/4-strings/strings-replace.js
@@ -0,0 +1,7 @@
+/*
+STRING REPLACE
+
+.replace(string, replaceString) replaces a specified string within a string variable with another string.
+'Wizard of Oz'.replace('Wizard','Witch) //'Witch of Oz'
+
+*/
