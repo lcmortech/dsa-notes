f0cfd2b7e10229a11a499983214f67967afcd721
Lauren Morgan <rg326@live.com>
2022-09-01 21:55:15 -0400

    minor edit

--- bae-dsa/4-strings/strings-replace.js
+++ bae-dsa/4-strings/strings-replace.js
@@ -3,5 +3,4 @@ STRING REPLACE
 
 .replace(string, replaceString) replaces a specified string within a string variable with another string.
 'Wizard of Oz'.replace('Wizard','Witch) //'Witch of Oz'
-
 */
