3f7593de7e33b67a9dfab57453d02aed3fdf8388
Lauren Morgan <rg326@live.com>
2022-09-03 20:55:23 -0400

    added basic regex to bae dsa

---
+++ bae-dsa/4-strings/7-strings-basic-regex.js
@@ -0,0 +1,12 @@
+/*
+BASIC REGEX
+Here are the basic regex rules:
+
+^: Indicates the start of a string/line 
+\d: Finds any digit
+[abc]: Finds any character between the brackets
+[^abc]: Finds any character not between the brackets
+[0-9]: Finds any digit between the brackets
+[^0-9]: Finds any digit not between the brackets
+(x|y): Finds any of the alternatives specified
+*/
