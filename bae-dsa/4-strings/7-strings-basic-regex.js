dca5b501091b31e014dd1acc8d0461303308a082
Lauren Morgan <rg326@live.com>
2022-09-03 21:10:24 -0400

    added basic regex example to bae dsa

--- bae-dsa/4-strings/7-strings-basic-regex.js
+++ bae-dsa/4-strings/7-strings-basic-regex.js
@@ -9,4 +9,9 @@ Here are the basic regex rules:
 [0-9]: Finds any digit between the brackets
 [^0-9]: Finds any digit not between the brackets
 (x|y): Finds any of the alternatives specified
+
+The following returns 11, which is the index of the character D, which is the first character of the matched regex:
+1 - let str = 'Javascript DataStructures'
+2 - let n = str.search(/DataStructures/)
+3 - console.log(n) //prints '11'
 */
