e78182db1f11a772d4c6272c85bbadef4a6117e1
Lauren Morgan <rg326@live.com>
2022-09-05 16:32:42 -0400

    formatted common regex notes

--- bae-dsa/4-strings/8-strings-common-regex.js
+++ bae-dsa/4-strings/8-strings-common-regex.js
@@ -1,18 +1,19 @@
 /*
-
+COMMONLY USED REGEXES
 Regexes are immensely helpful for checking the validity of user input in JavaScript. One common type of input check is to validate whether it has any numeric characters.
 The following are five regexes that developers often use.
 Chapter 4 JavaSCript StringS
 39
 
-Chapter 4 JavaSCript StringS
-Any Numeric Characters
+
+//Any Numeric Characters
 /\d+/
 1 var reg = /\d+/;
 2 reg.test("123"); // true
 3 reg.test("33asd"); // true
 4 reg.test("5asdasd"); // true 5 reg.test("asdasd"); // false
-Only Numeric Characters
+
+//Only Numeric Characters
 /^\d+$/
 1 var reg = /^\d+$/;
 2 reg.test("123"); // true
@@ -21,7 +22,8 @@ Floating Numeric Characters
    /^[0-9].[0-9][1-9]+$/
 1 var reg = /^[0-9].[0-9][1-9]+$/; 2 reg.test("12"); // false
 3 reg.test("12.2"); // true
-Only Alphanumeric Characters
+
+//Only Alphanumeric Characters
    /[a-zA-Z0-9]/
 1 var reg = /[a-zA-Z0-9]/;
 2 reg.test("somethingELSE"); // true 3 reg.test("hello"); // true
