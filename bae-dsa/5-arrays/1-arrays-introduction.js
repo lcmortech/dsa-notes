4fb59a9fa32ca57905996366c01a9eca81f8732d
Lauren Morgan <rg326@live.com>
2022-09-07 20:56:19 -0400

    added new array chapter and intro in bae dsa

---
+++ bae-dsa/5-arrays/1-arrays-introduction.js
@@ -0,0 +1,9 @@
+/*
+INTRODUCING ARRAYS
+Arrays are one of the most fundamental data structures. If you have ever programmed before, you've most likely used an array.
+let array1 = [1,2,3,4];
+
+For any data structure, developers are interested in time and space complexity associated with the four fundamental operations: 
+access, insertion, deletion, and search.
+(For a review of Big-O notations, please refer to Chapter 1).
+*/
