0b65f4483b5aae02c6597c01c3142767c35cae2f
Lauren Morgan <rg326@live.com>
2022-09-08 20:29:22 -0400

    added final notes for insertion for arrays in bae dsa

--- bae-dsa/5-arrays/2-arrays-insertion.js
+++ bae-dsa/5-arrays/2-arrays-insertion.js
@@ -1,8 +1,11 @@
 /*
 INSERTION
 Insertion means adding a new element inside a data structure. Javascript implements array insertion with the .push(element) method. This method adds a new element at the end of the array.
+
 let array1 = [1,2,3,4]
 array1.push(5) //array1 = [1,2,3,4,5]
 array1.push(7) //array1 = [1,2,3,4,5,7]
 array1.push(2) //array1 = [1,2,3,4,5,7,2]
+
+The time complexity of this operation is O(1) in theory. It sahould be noted that, practically, this depends on the Javascript engine that runs the code. This applies to all natively supported Javascript objects.
 */
