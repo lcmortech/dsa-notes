9798b8a91f7aa2a906ebb077765df3b4bd0981b0
Lauren Morgan <rg326@live.com>
2022-09-08 20:27:48 -0400

    added insertion to bae dsa

---
+++ bae-dsa/5-arrays/2-arrays-insertion.js
@@ -0,0 +1,8 @@
+/*
+INSERTION
+Insertion means adding a new element inside a data structure. Javascript implements array insertion with the .push(element) method. This method adds a new element at the end of the array.
+let array1 = [1,2,3,4]
+array1.push(5) //array1 = [1,2,3,4,5]
+array1.push(7) //array1 = [1,2,3,4,5,7]
+array1.push(2) //array1 = [1,2,3,4,5,7,2]
+*/
