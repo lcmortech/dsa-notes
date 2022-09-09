aebd4b35ded20752c2a20c4b06dd16540f3ddf8c
Lauren Morgan <rg326@live.com>
2022-09-09 19:57:48 -0400

    added deletion to arrays in bae dsa

---
+++ bae-dsa/5-arrays/3-arrays-deletion.js
@@ -0,0 +1,13 @@
+/*
+DELETION
+Javascript implements array deletion with .pop() method. This method removes the last-added element of the array. This also returns the removed element.
+
+let array1 = [1,2,3,4]
+array1.pop() //returns 4, array = [1,2,3]
+array1.pop() //returns 3, array1 = [1,2]
+
+The time complexity of .pop() is O(1) similarly to .push. Another way to remove an element from an array is with the .shift() method. This method will remove the first element and return it.
+
+array1 = [1,2,3,4]
+array1.shift() //returns 2, array1 = [3,4]
+*/
