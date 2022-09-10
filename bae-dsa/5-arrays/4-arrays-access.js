a580f1441aa3f771eab73482729084cbda44edc9
Lauren Morgan <rg326@live.com>
2022-09-09 20:02:02 -0400

    added access to arrays in bae dsa

---
+++ bae-dsa/5-arrays/4-arrays-access.js
@@ -0,0 +1,6 @@
+/*
+ACCESS
+Accessing an array at a specified index only takes O(1) because this process uses that indes to get the value directly from the address in memory. It is done by specifying the index (remember that indexing starts at 0).
+let array1 = [1,2,3,4]
+array[1] //returns 2
+*/
