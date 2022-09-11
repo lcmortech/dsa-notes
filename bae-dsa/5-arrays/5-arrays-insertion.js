5df39090a9fcc760b9e515da72f0ec75d5c3b3e4
Lauren Morgan <rg326@live.com>
2022-09-10 21:04:23 -0400

    added insertion to bae dsa

---
+++ bae-dsa/5-arrays/5-arrays-insertion.js
@@ -0,0 +1,4 @@
+/*
+Iteration
+Iteration is the process of accessing each of the items contained within a data structure. There are multiple ways to iterate through an array in JavaScript. They all have a time complexity of O(n) since the iteration is visiting n number of elements.
+*/
