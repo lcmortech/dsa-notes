05b228a5a8494b9b0b30c9ed0aa67dd253ac908f
Lauren Morgan <rg326@live.com>
2022-09-10 21:11:20 -0400

    added for loop iteration to bae dsa

--- bae-dsa/5-arrays/5-arrays-insertion.js
+++ bae-dsa/5-arrays/5-arrays-insertion.js
@@ -1,4 +1,9 @@
 /*
 Iteration
 Iteration is the process of accessing each of the items contained within a data structure. There are multiple ways to iterate through an array in JavaScript. They all have a time complexity of O(n) since the iteration is visiting n number of elements.
+
+for (Variables; Condition; Modification)
+for is the most common method of iteration. It is most often used in this form: 1 for ( var i=0, len=array1.length; i<len; i++ ) {
+2 3}
+console.log(array1[i]);
 */
