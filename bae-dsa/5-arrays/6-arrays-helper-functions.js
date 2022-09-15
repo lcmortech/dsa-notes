0da900557d9822f176dbe1bf66f51be49b41ec3e
Lauren <rg326@live.com>
2022-09-14 20:01:47 -0400

    added slice method to arrays in bae dsa

---
+++ bae-dsa/5-arrays/6-arrays-helper-functions.js
@@ -0,0 +1,13 @@
+/*
+HELPER FUNCTIONS
+
+The following sections discuss other commonly used helper functions for processing. In addition, working with arrays will be covered.
+
+//.slice(begin, end)
+This helper function returns a portion of an existing array. .slice takes two parameters: the beginning index and the ending index of the array.
+
+let array1 = [1,2,3,4]
+array1.slice(1,2) //returns 2
+array1.slice(2,4) //returns [3,4]
+array1 = [1,2,3,4]
+*/
