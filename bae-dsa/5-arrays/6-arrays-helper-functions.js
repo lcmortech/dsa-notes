b30598922a92f1404db9c286aac298b6bcdc3c98
Lauren <rg326@live.com>
2022-09-17 14:48:34 -0400

    added splice method examples to arrays in bae dsa

--- bae-dsa/5-arrays/6-arrays-helper-functions.js
+++ bae-dsa/5-arrays/6-arrays-helper-functions.js
@@ -58,4 +58,24 @@ array2 //[5,2,3,4]
 This helper function returns and changes the contents of an array by removing existing elements, and/or adding new elements.
 
 .splice() takes three parameters: the beginning index, the size of things to be removed, and the new elements to add. New elements are added at the position specified by the first parameter. It returns the removed elements.
+
+let array1 = [1,2,3,4]
+array2.splice() //returns []
+array1 = [1,2,3,4]
+array1.splice(1,2) //returns [2,3]
+
+This example demonstrates removal. [2,3] was returned because it selected two items starting from an index of 1.
+
+let array1 = [1,2,3,4]
+array1.splice() //returns []
+array1.splice(1,2,5,6,7) returns [2,3]
+array1 = [1,5,6,7,4]
+
+Anything (any object type) can be added to the array. This is the beauty (and odd part) of Javascript.
+
+let array1 = [1,2,3,4]
+array1.splice(1,2[5,6,7]) //returns [2,3]
+array1 = [1,[5,6,7],4]
+
+.splice() is, worst case, O(n). Similarly to copying, if the range specified is the whole array, each n item has to be removed.
 */
