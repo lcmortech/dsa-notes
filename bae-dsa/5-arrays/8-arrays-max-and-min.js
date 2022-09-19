3f2081e110ec0dff5f675f7f32986086e8be904c
Lauren <rg326@live.com>
2022-09-19 19:56:33 -0400

    added math max and min methods to arrays in bae dsa

---
+++ bae-dsa/5-arrays/8-arrays-max-and-min.js
@@ -0,0 +1,15 @@
+/*
+//MAX AND MIN
+//Math.max and Math.min
+Both the Math.max and Math.min functions take an unlimited number of parameters, so you can use the spread operator for the following operations.
+
+To find the maximum in an array, use this:
+
+let array1 = [1,2,3,4,5]
+Math.max(array1) // 5
+
+To find the minimum array, use this:
+
+let array2 = [3,2,-123,2132,12]
+Math.min(array2) // -123
+*/
