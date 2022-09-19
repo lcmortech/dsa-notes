7a72607e9a263c7fe52a6d72014d872e085c630c
Lauren <rg326@live.com>
2022-09-19 19:56:33 -0400

    added example for spread operator in arrays in bae dsa

--- bae-dsa/5-arrays/7-arrays-spread-operator.js
+++ bae-dsa/5-arrays/7-arrays-spread-operator.js
@@ -1,3 +1,14 @@
 /*
 //SPREAD OPERATOR
+The spread operator, denoted by 3 periods (...), is used to expand arguments where zero arguments are expected.
+
+function addFourNums(a,b,c,d) {
+    return a + b + c + d
+}
+let numbers = [1,2,3,4]
+
+console.log(addFourNumbers(...numbers)) //10
+
+//Math.max and Math.min
+Both the Math.max and Math.min functions take an unlimited number of parameters, so you can use the spread operator for the following operations.
 */
