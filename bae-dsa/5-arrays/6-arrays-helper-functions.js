83a551db2f1c8225baf3a7a1436b8626e0768c0f
Lauren <rg326@live.com>
2022-09-18 20:43:28 -0400

    added examples for concat in arrays in bae dsa

--- bae-dsa/5-arrays/6-arrays-helper-functions.js
+++ bae-dsa/5-arrays/6-arrays-helper-functions.js
@@ -81,4 +81,12 @@ array1 = [1,[5,6,7],4]
 
 //.concat()
 This adds new elements to the array at the end of the array and returns the array.
+
+let array1 = [1,2,3,4]
+array1.concat() //returns [1,2,3,4]
+array1.concat([2,3,4]) //returns [1,2,3,4,2,3,4]
+array1 = [1,2,3,4]
+
+//.length Property
+The .length proper
 */
