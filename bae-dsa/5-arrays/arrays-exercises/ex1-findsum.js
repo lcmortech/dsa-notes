c96264a779b65f57d518ed28f2b4014e9b522bea
Lauren <rg326@live.com>
2022-09-22 23:12:09 -0400

    findsum edit in arrays in bae dsa

--- bae-dsa/5-arrays/arrays-exercises/ex1-findsum.js
+++ bae-dsa/5-arrays/arrays-exercises/ex1-findsum.js
@@ -20,6 +20,11 @@ This solution iterates through an array looking to see whether a matching pair e
 Two for loops over n elements of the array yields a high time complexity. However, no extra memory was created. Similar to how time complexity describes the time required relative to input size, n, to finish the algorithm, the space complexity describes the additional memory needed for implementation. The space complexity, O(1), is constant.
 Time Complexity: O(n2)
 Space Complexity: O(1)
+
+Let's think about how to do this in lineat time of O(n).
+What if any previously seen array elements were stored and could be checked easily?
+Here's the input:
+
 */
 
 findSum([1,2,3,4],7)
