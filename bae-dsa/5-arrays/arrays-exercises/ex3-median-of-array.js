5fe96e05cd6dd58e24eb744df5eabb6d171943c4
Lauren <rg326@live.com>
2022-09-24 19:31:14 -0400

    added explanation of median of two arrays in arrays in bae dsa

--- bae-dsa/5-arrays/arrays-exercises/ex3-median-of-array.js
+++ bae-dsa/5-arrays/arrays-exercises/ex3-median-of-array.js
@@ -1,4 +1,5 @@
 /*
-
-
+//Find the Median of Two Sorted Arrays of the Same Size
+Recall the median in an even number of a set is the average of the two middle numbers. If the array is sorted, this is simple.
+Here is an example: [1,2,3,4] has the median of (2+3)/2 = 2.5
 */
