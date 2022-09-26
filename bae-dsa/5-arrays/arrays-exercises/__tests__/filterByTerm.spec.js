efdcb74fac8d726bc069222f26265d2901c22d21
Lauren <rg326@live.com>
2022-09-25 21:47:27 -0400

    added final note to filterbyterm

--- bae-dsa/5-arrays/arrays-exercises/__tests__/filterByTerm.spec.js
+++ bae-dsa/5-arrays/arrays-exercises/__tests__/filterByTerm.spec.js
@@ -1,6 +1,13 @@
 describe('filter funtion',() => {
     test('it should filter by a search term(link)',() => {
+        const input = [
+            {id: 1, url: 'https://www.url1.dev'},
+            {id: 2, url: 'https://www.url2.dev'},
+            {id: 3, url: 'https://www.link3.dev'}
+        ]
         //actual test
     })
     //test stuff
 })
+
+//accidentally pushed github changes early 9-25-2022
