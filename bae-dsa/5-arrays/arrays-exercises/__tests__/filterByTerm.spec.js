c8c879149616b229056824996bdccea55c1b1f2a
Lauren <rg326@live.com>
2022-09-26 21:43:22 -0400

    added filterbyterm function to filtebyterm.spec in arrays in bae dsa

--- bae-dsa/5-arrays/arrays-exercises/__tests__/filterByTerm.spec.js
+++ bae-dsa/5-arrays/arrays-exercises/__tests__/filterByTerm.spec.js
@@ -1,3 +1,9 @@
+function filterByTerm(inputArr, searchTerm){
+    return inputArr.filter(function(arrayElement){
+        return arrayElement.url.match(searchTerm)
+    })
+}
+
 describe('filter funtion',() => {
     test('it should filter by a search term(link)',() => {
         const input = [
