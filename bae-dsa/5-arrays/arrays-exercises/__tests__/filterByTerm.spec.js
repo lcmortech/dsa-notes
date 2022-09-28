c87eb0a8cc0cb7214083746545de5470c4258728
Lauren <rg326@live.com>
2022-09-28 07:37:34 -0400

    instantiated new regex for filterbyterm file in arrays in bae dsa

--- bae-dsa/5-arrays/arrays-exercises/__tests__/filterByTerm.spec.js
+++ bae-dsa/5-arrays/arrays-exercises/__tests__/filterByTerm.spec.js
@@ -17,6 +17,7 @@ describe('filter function',() => {
 })
 
 function filterByTerm(inputArr, searchTerm){
+    const regex = new RegExp(searchTerm, 'i')
     return inputArr.filter(function(arrayElement){
         return arrayElement.url.match(regex)
     })
