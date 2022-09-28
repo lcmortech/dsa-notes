b6433bea7a2e1389907e6aa2c072fae2925da24e
Lauren <rg326@live.com>
2022-09-27 22:42:06 -0400

    changed ‘searchTerm’ to ‘regex’ in filterbyterm spec file in arrays in bae dsa

--- bae-dsa/5-arrays/arrays-exercises/__tests__/filterByTerm.spec.js
+++ bae-dsa/5-arrays/arrays-exercises/__tests__/filterByTerm.spec.js
@@ -1,6 +1,6 @@
 function filterByTerm(inputArr, searchTerm){
     return inputArr.filter(function(arrayElement){
-        return arrayElement.url.match(searchTerm)
+        return arrayElement.url.match(regex)
     })
 }
 
